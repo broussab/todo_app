c82636fd250ead99ee105a7aa0931670fcc0c350
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2016-06-08 17:21:31 -0500

    All of the changes from part 2 of the tutorial.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ActiveRecord::Base
+  validates_presence_of :title
+  validate :future_completed_date
+  
+ private
+ 
+ def future_completed_date
+  if !completed.blank? && completed > Date.today
+      self.errors.add(:completed, "Can't be a Future Date")
+  end
+ end
 end
